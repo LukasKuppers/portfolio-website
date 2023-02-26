9a893dd8b88290a7ab093b4e96d23df308d9af4a
Lukas Kuppers <lkuppers11@gmail.com>
2023-02-26 14:41:13 -0500

    Add nav menu

--- app/src/components/MenuBar.jsx
+++ app/src/components/MenuBar.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+const colors = require('../colors.json');
 
 const MenuBar = ({ fontColor }) => {
+
+    const [navOpen, setNavOpen] = useState(false);
+
     return (
         <div className="menu-bar">
             <h1 className={fontColor}>LUKAS K.</h1>
+            <FontAwesomeIcon 
+                icon={solid('bars')} 
+                className={`menu-icon ${fontColor}`} size="xl"
+                onClick={() => setNavOpen(!navOpen)} />
+            <div className={`menu-nav${navOpen ? ' show-menu-nav' : ''} ${colors.bg_accent}`}>
+                <ul className='menu-nav-list'>
+                    <li><a className="nav-link" href="home">HOME</a></li>
+                    <li><a className="nav-link" href="blah">BLAH</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
