86a3d0ebe3f81f91277d5ecd3be264ee11b1e4a1
Lukas Kuppers <lkuppers11@gmail.com>
2023-02-26 20:08:42 -0500

    Add menu bar to not found apge

--- app/src/pages/NotFoundPage.jsx
+++ app/src/pages/NotFoundPage.jsx
@@ -1,7 +1,10 @@
+import MenuBar from "../components/MenuBar";
+const colors = require('../colors.json');
 
 const NotFoundPage = () => {
     return (
         <div className="page-not-found">
+            <MenuBar fontColor={colors.dark}/>
             <h1>Page Doesn't Exist!</h1>
         </div>
     )
