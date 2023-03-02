095b733ed83a9e5068b74b917f7213731167b415
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-01 22:54:30 -0500

    adding back harmless key

--- app/src/util/GoogleSheets.js
+++ app/src/util/GoogleSheets.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// The sheet is public, so this isn't a big deal
+const KEY = 'AIzaSyCXkA23LzBdx5ywUObyg0nAo9MpX-UziYI';
 const SHEETS_API_ENDPOINT = 'https://sheets.googleapis.com';
 const SPREADSHEET_ID = '1-WimflwtcXox3iJqiP9lOMXap2r-Gw9iemmZA26XGIg';
 
@@ -14,7 +16,7 @@ const getSpreadsheetRows = async (range) => {
 };
 
 const generateValuesGetRequestString = (range) => {
-    return `${SHEETS_API_ENDPOINT}/v4/spreadsheets/${SPREADSHEET_ID}/values/${range}?key=${API_KEY}`;
+    return `${SHEETS_API_ENDPOINT}/v4/spreadsheets/${SPREADSHEET_ID}/values/${range}?key=${KEY}`;
 };
 
 export default getSpreadsheetRows;
