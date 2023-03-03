09e3cb496b50737c16cea06e3f39ac1ac05d192f
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-02 19:44:16 -0500

    make xp layout reactive to width

--- app/src/pages/SoftwareDevPage.jsx
+++ app/src/pages/SoftwareDevPage.jsx
@@ -10,11 +10,15 @@ const text = require('../textChunks.json');
 const EXPERIENCE_SHEET_ID = 'technical-experience';
 const PROJECTS_SHEET_ID = 'technical-projects';
 
+const XP_COL_THRESHOLD = 1400;
+
 const SoftwareDevPage = () => {
 
     const [experienceData, setexperienceData] = useState([]);
     const [projectsData, setProjectsData] = useState([]);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+    // fetch data on initial render
     useEffect(() => {
         const fetchData = async () => {
             const fetchedExperienceData = await getSpreadsheetDataAsJson(EXPERIENCE_SHEET_ID);
@@ -25,13 +29,25 @@ const SoftwareDevPage = () => {
         fetchData();
     }, []);
 
+    // update window width on resize
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    });
+
     return (
         <div className='page-home'>
             <MenuBar fontColor={colors.light} />
             <SocialsMenu color={colors.light} />
             <div className='dev-body'>
                 <h1 className={colors.light}>Industry Experience</h1>
-                <div>
+                <div className={windowWidth < XP_COL_THRESHOLD ? 'flex-col' : 'flex-row'}>
                     {experienceData.map(xp => <DevXpContainer key={xp.company} xpData={xp} />)}
                 </div>
                 <h1 className={colors.light}>Technical Projects</h1>
