759a5690174b50768a80637658d711163ecfa241
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-06 18:39:11 -0500

    Finish contact apge

--- app/src/pages/ContactPage.jsx
+++ app/src/pages/ContactPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import MenuBar from '../components/MenuBar';
 import SocialsMenu from '../components/SocailsMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 const colors = require('../colors.json');
 const text = require('../textChunks.json');
 
@@ -12,6 +14,15 @@ const ContactPage = () => {
             <div className='contact-body'>
                 <h1 className={colors.dark}>Contact</h1>
                 <p className={colors.dark}>{text.contactBody}</p>
+                <div className='contact-row'>
+                    <FontAwesomeIcon icon={brands('linkedin-in')} size='xl' className={colors.dark} />
+                    <a href='https://www.linkedin.com/in/lukas-kuppers-a92469110/'
+                        className={colors.accent}>&gt; LINKEDIN</a>
+                </div>
+                <div className='contact-row'>
+                    <FontAwesomeIcon icon={solid('envelope')} size='xl' className={colors.dark} />
+                    <a href='' className={colors.accent}>LKUPPERS11@GMAIL.COM</a>
+                </div>
             </div>
         </div>
     );
