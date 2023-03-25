a2ca14ab423f50b00c02ab534f02b05d044fbcec
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-24 22:04:43 -0400

    adjust styling on art page

--- app/src/pages/ArtPage.jsx
+++ app/src/pages/ArtPage.jsx
@@ -4,6 +4,7 @@ import SocialsMenu from '../components/SocailsMenu';
 import ArtImageContainer from '../components/ArtImageContainer';
 import getSpreadsheetDataAsJson from '../util/GoogleSheets';
 const colors = require('../colors.json');
+const text = require('../textChunks.json');
 
 const MISC_SPREADSHEET_ID = 'static-art-images';
 
@@ -26,7 +27,7 @@ const ArtPage = () => {
             <MenuBar fontColor={colors.dark} bgColor={colors.bg_light} />
             <SocialsMenu excludeExternalLinks={true} color={colors.dark} />
             <div className='art-body'>
-                <h1 className={colors.dark}>Art</h1>
+                <h1 className={colors.dark}>{text.artPageHeader}</h1>
                 <div className='art-images-container'>
                 {images.map(imgData => <ArtImageContainer key={imgData.name} imageData={imgData} />)}
                 </div>
