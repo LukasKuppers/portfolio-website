ff0929039b0e009445f46d0688d2d461f0e17960
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-24 23:17:16 -0400

    sort images by date

--- app/src/pages/ArtPage.jsx
+++ app/src/pages/ArtPage.jsx
@@ -16,6 +16,15 @@ const ArtPage = () => {
         const fetchData = async () => {
             const miscData = await getSpreadsheetDataAsJson(MISC_SPREADSHEET_ID);
             if (miscData && miscData.length > 0) {
+                // sort images by provided date (desc)
+                miscData.sort((a, b) => {
+                    let dateA = new Date(a.date);
+                    let dateB = new Date(b.date);
+                    if (dateA < dateB) return 1;
+                    if (dateA > dateB) return -1;
+                    return 0;
+                });
+
                 setImages(miscData);
             }
         };
