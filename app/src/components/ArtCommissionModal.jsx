2f960063c0172929bf7948be7059f91c03c92504
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-30 11:50:13 -0400

    enable art modal close button

--- app/src/components/ArtCommissionModal.jsx
+++ app/src/components/ArtCommissionModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 const colors = require('../colors.json');
@@ -6,18 +6,25 @@ const text = require('../textChunks.json');
 
 const ArtCommissionModal = ({ bgColor }) => {
 
+    const [open, setOpen] = useState(true);
+
     const textColor = (backgroundColor) => {
         return backgroundColor === colors.bg_dark ? colors.light : colors.dark;
     }
 
+    const closeModal = () => {
+        setOpen(false);
+    }
+
     return (
-        <div className={`art-commission-modal ${bgColor}`}>
+        <div className={`art-commission-modal ${bgColor} ${open ? '' : 'disp-none'}`}>
             <div className='art-commission-modal-header'>
                 <h1 className={textColor(bgColor)}>{text.artCommissionPrompt}</h1>
                 <FontAwesomeIcon 
                     icon={solid('circle-xmark')}
                     size='2xl'
-                    className={textColor(bgColor)} />
+                    className={`art-commission-modal-close-icon ${textColor(bgColor)}`}
+                    onClick={closeModal}/>
             </div>
             <span className={textColor(bgColor)}>{text.artCommissionInfo}</span>
             <a href='contact' className={colors.accent}>&gt; CONTACT</a>
