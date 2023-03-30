d8ac826e66a7b604cfd25cd7c05a8e1469cb5ba5
Lukas Kuppers <lkuppers11@gmail.com>
2023-03-30 19:29:40 -0400

    Dynamically change document title

--- app/src/Router.jsx
+++ app/src/Router.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import pageRouteMap from "./pageRouteMap";
 import NotFoundPage from "./pages/NotFoundPage";
+const pageNameMap = require('./pageNameMap.json');
 
 const Router = () => {
+
+    useEffect(() => {
+        const page = window.location.pathname;
+        const pageName = page !== null ? `Lukas Kuppers | ${pageNameMap[page]}`: '';
+        document.title = pageName;
+    }, []);
 
     const getPage = () => {
         const page = pageRouteMap[window.location.pathname];
