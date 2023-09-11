71693eaeba42019936a6d29b4f9556f0988d7f24
Lukas Kuppers <lkuppers11@gmail.com>
2023-09-11 12:20:52 -0700

    Add headshot to home page

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MenuBar from '../components/MenuBar';
 import SocialsMenu from '../components/SocailsMenu';
+import headshot from '../headshot.png';
 const colors = require('../colors.json');
 const text = require('../textChunks.json');
 
 const HomePage = () => {
+
+    const imageRef = useRef(null);
+    const textContainerRef = useRef(null);
+
+    useEffect(() => {
+        // get height of header text container:
+        const textContainerHeight = textContainerRef.current.clientHeight;
+
+        // Set headshot height to match text containers height:
+        imageRef.current.style.height = `${textContainerHeight}px`;
+    }, []);
+
     return (
         <div className='page-home'>
             <MenuBar fontColor={colors.light} bgColor={colors.bg_dark} />
             <SocialsMenu color={colors.light} />
             <div className='home-intro-section'>
-                <span className={colors.accent}>👋 Hi, I'm</span>
-                <h1 className={colors.light}>Lukas Kuppers</h1>
+                <div className='home-header-container'> 
+                    <div className='home-header-img' ref={imageRef}>
+                        <img src={headshot} alt='profile headshot' />
+                    </div>
+                    <div className='home-header-text' ref={textContainerRef}>
+                        <span className={colors.accent}>👋 Hi, I'm</span>
+                        <h1 className={colors.light}>Lukas Kuppers</h1>
+                    </div>
+                </div>
                 <p className={colors.medium}>{text.professionalIntro}</p>
                 <p className={colors.medium}>{text.personalIntro}</p>
                 <div className='home-link-container'>
